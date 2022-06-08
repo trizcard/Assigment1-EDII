7638ce494d97bfb9bcfa80e90f273b98d35c5607
trizcard <beatrizco@usp.br>
2022-06-08 09:31:00 -0300

    comentando o cód + arrumando bug "maior digito"

--- main.c
+++ main.c
@@ -3,21 +3,22 @@
 #include "ordena.h"
 
 int main(){
-    int n;
+    int n; 
     scanf("%d", &n);
+
     int **A;
-    A = malloc (n * sizeof(int*));
+    A = malloc (n * sizeof(int*)); // aloca memória de acordo com quantas linhas tem a matriz
 
     for (int i = 0; i < n; i++){
-        A[i] = malloc (2 * sizeof(int));
-        scanf("%d %d", &A[i][0], &A[i][1]);
+        A[i] = malloc (2 * sizeof(int)); // aloca memória para 2 colunas
+        scanf("%d %d", &A[i][0], &A[i][1]); 
     }
     
     ordenaNumeros(A, n);
     
     for (int i = 0; i < n; i++){
-        printf("%d %d\n", A[i][0], A[i][1]);
-        free(A[i]);
+        printf("%d %d\n", A[i][0], A[i][1]); // imprime a matriz ordenada
+        free(A[i]); // libera memória das colunas
     }
-    free(A);
+    free(A); // libera memória das linhas
 }
--- ordena.c
+++ ordena.c
@@ -6,45 +6,47 @@
 void ordenaDigitos (int **A, int n, int posicao){
     int B[10];
     for (int i = 0; i < 10; i++){
-        B[i] = 0;
+        B[i] = 0; // zera todos os valores de B
     }
+
     int digito;
     for (int i = 0; i < n; i++){
         digito = A[i][0]/posicao;
-        digito = digito % 10;
-        B[digito] = B[digito] + 1;
+        digito = digito % 10; // seleciona o algarismo da posição desejada
+        B[digito] = B[digito] + 1; // soma quantos números tem esse determinado algarismo
     }
 
     for (int i = 1; i < 10; i++){
-        B[i] = B[i] + B[(i-1)];
+        B[i] = B[i] + B[(i-1)];  // atribui o atual como a soma de si mais o anterior 
     }
 
     int C[n][2];
 
     for (int i = (n-1); i >= 0; i--){
         digito = A[i][0]/posicao;
-        digito = digito % 10;
-        B[digito] = B[digito] - 1;
-        C[B[digito]][0] = A[i][0];
+        digito = digito % 10; // seleciona o algarismo da posição desejada
+        B[digito] = B[digito] - 1; 
+        C[B[digito]][0] = A[i][0]; // coloca na menor posição de uma matriz temporária o numero com dígito menor
         C[B[digito]][1] = A[i][1];
     }
 
     for (int i = 0; i < n; i++){
-        A[i][0] = C[i][0];
+        A[i][0] = C[i][0]; // passa da matriz temporária para a matriz A
         A[i][1] = C[i][1];
     }
 }
 
 void ordenaNumeros(int **A, int n){
-    int maior = A[0][0];
+    int maior = A[0][0]; // atribui como se o maior já fosse o primeiro número
     for (int i = 1; i < n; i++){
-        if (A[i][0] > A[i-1][0]){
+        if (A[i][0] > maior){ // testa se o da posição atual é maior que o maior já achado
             maior = A[i][0];
         }
     }
     int posicao = 1;
-    while(maior/posicao > 0){
-        ordenaDigitos(A, n, posicao);
-        posicao = posicao * 10;
+    
+    while(maior/posicao > 0){ // enquanto a posição não chega ao número com maior algarismos
+        ordenaDigitos(A, n, posicao); 
+        posicao = posicao * 10; // muda para a próxima posição
     }
 }
